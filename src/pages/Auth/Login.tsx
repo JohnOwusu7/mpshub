d8e2e6cc1a9a2d2faaff3f282a9cc2614ce6f959
John Owusu <qaredadev@gmail.com>
2024-07-01 10:43:46 +0000

    hosting

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -70,15 +70,14 @@ const Login = () => {
     return (
         <div>
             <div className="absolute inset-0">
-                <img src="/assets/images/auth/bg-gradient.png" alt="image" className="w-full h-full object-cover" />
+                <img src="/assets/images/auth/network.jpg" alt="image" className="w-full h-full object-cover" />
             </div>
             <div className="relative flex min-h-screen items-center justify-center bg-[url(/assets/images/auth/map.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
                 <img src="/assets/images/auth/coming-soon-object1.png" alt="image" className="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
                 <img src="/assets/images/auth/coming-soon-object2.png" alt="image" className="absolute left-24 top-0 h-40 md:left-[30%]" />
                 <img src="/assets/images/auth/coming-soon-object3.png" alt="image" className="absolute right-0 top-0 h-[300px]" />
-                <img src="/assets/images/auth/polygon-object.svg" alt="image" className="absolute bottom-0 end-[28%]" />
                 <div className="relative flex w-full max-w-[1502px] flex-col justify-between overflow-hidden rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 lg:min-h-[758px] lg:flex-row lg:gap-10 xl:gap-0">
-                    <div className="relative hidden w-full items-center justify-center bg-[linear-gradient(225deg,rgba(239,18,98,1)_0%,rgba(67,97,238,1)_100%)] p-5 lg:inline-flex lg:max-w-[835px] xl:-ms-28 ltr:xl:skew-x-[14deg] rtl:xl:skew-x-[-14deg]">
+                    <div className="relative hidden w-full items-center justify-center  p-5 lg:inline-flex lg:max-w-[835px] xl:-ms-28 ltr:xl:skew-x-[14deg] rtl:xl:skew-x-[-14deg]">
                         <div className="absolute inset-y-0 w-8 from-primary/10 via-transparent to-transparent ltr:-right-10 ltr:bg-gradient-to-r rtl:-left-10 rtl:bg-gradient-to-l xl:w-16 ltr:xl:-right-20 rtl:xl:-left-20"></div>
                         <div className="ltr:xl:-skew-x-[14deg] rtl:xl:skew-x-[14deg]">
                             <div className="mt-24 hidden w-full max-w-[430px] lg:block">
@@ -137,7 +136,7 @@ const Login = () => {
                                 Request New
                             </Link>
                         </div>
-                        <p className="absolute bottom-6 w-full text-center dark:text-white">© {new Date().getFullYear()}.EIS JohnCode All Rights Reserved.</p>
+                        <p className="absolute bottom-6 w-full text-center dark:text-white">© {new Date().getFullYear()}.Mining Projects & Systems HUB All Rights Reserved.</p>
                     </div>
                 </div>
             </div>
--- src/pages/Auth/SignOut.tsx
+++ src/pages/Auth/SignOut.tsx
@@ -5,46 +5,30 @@ import React from 'react';
  */
 function SignOutPage() {
     return (
-        // <div className="flex items-center justify-center min-h-screen">
-        //     <div className="flex flex-col items-center">
-        //         <div className="w-full max-w-320 px-16 py-32 bg-white rounded-lg shadow-md">
-        //             <img
-        //                 className="mx-auto w-48"
-        //                 src="public/logo1.png"
-        //                 alt="logo"
-        //             />
-
-        //             <div className="mt-25 text-center text-4xl font-extrabold leading-tight tracking-tight">
-        //                 You have been signed out!
-        //             </div>
-        //             <a href='/auth' className='mt-10 text-4l leading-tight'>Sign In...</a>
-        //         </div>
-        //     </div>
-        // </div>
         <div>
             <div className="absolute inset-0">
-                <img src="/assets/images/auth/bg-gradient.png" alt="image" className="h-full w-full object-cover" />
+                <img src="/assets/images/auth/bg-gradient.png" alt="image" className="h-full w-full object-cover backdrop-brightness-50 bg-white/2" />
             </div>
 
             <div className="relative flex min-h-screen items-center justify-center bg-[url(/assets/images/auth/map.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
-                <img src="/assets/images/auth/coming-soon-object1.png" alt="image" className="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
-                <img src="/assets/images/auth/coming-soon-object2.png" alt="image" className="absolute left-24 top-0 h-40 md:left-[30%]" />
+                <img src="/assets/images/auth/smart.png" alt="image" className="absolute left-0 top-2/3 h-full max-h-[423px] -translate-y-1/2" />
+                <img src="/assets/images/auth/truck3.png" alt="image" className="absolute left-24 top-0 h-[300px] md:left-[15%]" />
                 <img src="/assets/images/auth/coming-soon-object3.png" alt="image" className="absolute right-0 top-0 h-[300px]" />
-                <img src="/assets/images/auth/polygon-object.svg" alt="image" className="absolute bottom-0 end-[28%]" />
-                <div className="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]">
-                    <div className="relative flex flex-col justify-center rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[758px] py-20">
+                <img src="/assets/images/auth/moving.png" alt="image" className="absolute top-[48%] end-[20%]" />
+                <div >
+                    <div className="relative flex flex-col justify-center rounded-md bg-white/30 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[250px] py-20">
                     <img
                         className="mx-auto w-48"
                         src="/logomain.png"
                         alt="logo"
                     />
-                    <div className="mt-25 text-center font-extra leading-tight">
+                    <div className="mt-25 text-center text-4xl font-extrabold leading-tight">
                          Mining Project and Systems HUB!
                      </div>
-                    <div className="mt-25 text-center text-4xl font-extrabold leading-tight tracking-tight">
-                         You have been signed out!
+                    <div className="mt-5 text-center text-4xl font-extra leading-tight tracking-tight">
+                         Welcome!
                      </div>
-                     <a href='/auth' className='text-center mt-10 text-4l leading-tight'>Sign In...</a>
+                     <a href='/auth' className='text-center mt-10 text-2xl tracking-tight leading-tight'>Sign In...</a>
                     </div>
                 </div>
             </div>
