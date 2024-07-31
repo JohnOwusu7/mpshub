aba6eabdfde99b2d66bec470c3ba630d3fc9ce98
John <johnowusuwan@gmail.com>
2024-07-31 19:24:22 +0000

    eseol

--- src/pages/safety/SafetyDashboard.tsx
+++ src/pages/safety/SafetyDashboard.tsx
@@ -13,159 +13,39 @@ import IconEye from '../../components/Icon/IconEye';
 const SafetyDashboard = () => {
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(setPageTitle('Invoice List'));
+        dispatch(setPageTitle('Safety Work Order'));
     });
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            invoice: '081451',
-            name: 'Laurie Fox',
-            email: 'lauriefox@company.com',
-            date: '15 Dec 2020',
-            amount: '2275.45',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 2,
-            invoice: '081452',
-            name: 'Alexander Gray',
-            email: 'alexGray3188@gmail.com',
-            date: '20 Dec 2020',
-            amount: '1044.00',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 3,
-            invoice: '081681',
-            name: 'James Taylor',
-            email: 'jamestaylor468@gmail.com',
-            date: '27 Dec 2020',
-            amount: '20.00',
-            status: { tooltip: 'Pending', color: 'danger' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 4,
-            invoice: '082693',
-            name: 'Grace Roberts',
-            email: 'graceRoberts@company.com',
-            date: '31 Dec 2020',
-            amount: '344.00',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 5,
-            invoice: '084743',
-            name: 'Donna Rogers',
-            email: 'donnaRogers@hotmail.com',
-            date: '03 Jan 2021',
-            amount: '405.15',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 6,
-            invoice: '086643',
-            name: 'Amy Diaz',
-            email: 'amy968@gmail.com',
-            date: '14 Jan 2020',
-            amount: '100.00',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 7,
-            invoice: '086773',
-            name: 'Nia Hillyer',
-            email: 'niahillyer666@comapny.com',
-            date: '20 Jan 2021',
-            amount: '59.21',
-            status: { tooltip: 'Pending', color: 'danger' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 8,
-            invoice: '087916',
-            name: 'Mary McDonald',
-            email: 'maryDonald007@gamil.com',
-            date: '25 Jan 2021',
-            amount: '79.00',
-            status: { tooltip: 'Pending', color: 'danger' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 9,
-            invoice: '089472',
-            name: 'Andy King',
-            email: 'kingandy07@company.com',
-            date: '28 Jan 2021',
-            amount: '149.00',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 10,
-            invoice: '091768',
-            name: 'Vincent Carpenter',
-            email: 'vincentcarpenter@gmail.com',
-            date: '30 Jan 2021',
-            amount: '400',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 11,
-            invoice: '095841',
-            name: 'Kelly Young',
-            email: 'youngkelly@hotmail.com',
-            date: '06 Feb 2021',
-            amount: '49.00',
-            status: { tooltip: 'Pending', color: 'danger' },
-            profile: 'profile-1.jpeg',
-        },
-        {
-            id: 12,
-            invoice: '098424',
-            name: 'Alma Clarke',
-            email: 'alma.clarke@gmail.com',
-            date: '10 Feb 2021',
-            amount: '234.40',
-            status: { tooltip: 'Paid', color: 'success' },
-            profile: 'profile-1.jpeg',
-        },
-    ]);
+    const defaultParams: any = {id: null, job: ''};
+    const [items, setItems] = useState([defaultParams]);
 
     const deleteRow = (id: any = null) => {
         if (window.confirm('Are you sure want to delete selected row ?')) {
-            if (id) {
-                setRecords(items.filter((user) => user.id !== id));
-                setInitialRecords(items.filter((user) => user.id !== id));
-                setItems(items.filter((user) => user.id !== id));
-                setSearch('');
-                setSelectedRecords([]);
-            } else {
-                let selectedRows = selectedRecords || [];
-                const ids = selectedRows.map((d: any) => {
-                    return d.id;
-                });
-                const result = items.filter((d) => !ids.includes(d.id as never));
-                setRecords(result);
-                setInitialRecords(result);
-                setItems(result);
-                setSearch('');
-                setSelectedRecords([]);
-                setPage(1);
-            }
+            // if (id) {
+            //     setRecords(items.filter((user) => user.id !== id));
+            //     setInitialRecords(items.filter((user) => user.id !== id));
+            //     setItems(items.filter((user) => user.id !== id));
+            //     setSearch('');
+            //     setSelectedRecords([]);
+            // } else {
+            //     let selectedRows = selectedRecords || [];
+            //     const ids = selectedRows.map((d: any) => {
+            //         return d.id;
+            //     });
+            //     const result = items.filter((d) => !ids.includes(d.id as never));
+            //     setRecords(result);
+            //     setInitialRecords(result);
+            //     setItems(result);
+            //     setSearch('');
+            //     setSelectedRecords([]);
+            //     setPage(1);
+            // }
         }
     };
 
     const [page, setPage] = useState(1);
     const PAGE_SIZES = [10, 20, 30, 50, 100];
     const [pageSize, setPageSize] = useState(PAGE_SIZES[0]);
-    const [initialRecords, setInitialRecords] = useState(sortBy(items, 'invoice'));
+    const [initialRecords, setInitialRecords] = useState(sortBy(items, 'job'));
     const [records, setRecords] = useState(initialRecords);
     const [selectedRecords, setSelectedRecords] = useState<any>([]);
 
@@ -190,7 +70,7 @@ const SafetyDashboard = () => {
         setInitialRecords(() => {
             return items.filter((item) => {
                 return (
-                    item.invoice.toLowerCase().includes(search.toLowerCase()) ||
+                    item.job.toLowerCase().includes(search.toLowerCase()) ||
                     item.name.toLowerCase().includes(search.toLowerCase()) ||
                     item.email.toLowerCase().includes(search.toLowerCase()) ||
                     item.date.toLowerCase().includes(search.toLowerCase()) ||
@@ -209,14 +89,14 @@ const SafetyDashboard = () => {
 
     return (
         <div className="panel px-0 border-white-light dark:border-[#1b2e4b]">
-            <div className="invoice-table">
+            <div className="job-table">
                 <div className="mb-4.5 px-5 flex md:items-center md:flex-row flex-col gap-5">
                     <div className="flex items-center gap-2">
                         <button type="button" className="btn btn-danger gap-2" onClick={() => deleteRow()}>
                             <IconTrashLines />
                             Delete
                         </button>
-                        <Link to="/apps/invoice/add" className="btn btn-primary gap-2">
+                        <Link to="/apps/job/add" className="btn btn-primary gap-2">
                             <IconPlus />
                             Add New
                         </Link>
@@ -228,15 +108,15 @@ const SafetyDashboard = () => {
 
                 <div className="datatables pagination-padding">
                     <DataTable
-                        className="whitespace-nowrap table-hover invoice-table"
+                        className="whitespace-nowrap table-hover job-table"
                         records={records}
                         columns={[
                             {
-                                accessor: 'invoice',
+                                accessor: 'job',
                                 sortable: true,
-                                render: ({ invoice }) => (
-                                    <NavLink to="/apps/invoice/preview">
-                                        <div className="text-primary underline hover:no-underline font-semibold">{`#${invoice}`}</div>
+                                render: ({ job }) => (
+                                    <NavLink to="/apps/job/preview">
+                                        <div className="text-primary underline hover:no-underline font-semibold">{`#${job}`}</div>
                                     </NavLink>
                                 ),
                             },
@@ -278,10 +158,10 @@ const SafetyDashboard = () => {
                                 textAlignment: 'center',
                                 render: ({ id }) => (
                                     <div className="flex gap-4 items-center w-max mx-auto">
-                                        <NavLink to="/apps/invoice/edit" className="flex hover:text-info">
+                                        <NavLink to="/apps/job/edit" className="flex hover:text-info">
                                             <IconEdit className="w-4.5 h-4.5" />
                                         </NavLink>
-                                        <NavLink to="/apps/invoice/preview" className="flex hover:text-primary">
+                                        <NavLink to="/apps/job/preview" className="flex hover:text-primary">
                                             <IconEye />
                                         </NavLink>
                                         {/* <NavLink to="" className="flex"> */}
